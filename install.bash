4ece80167cd35d3a204af52169b18c523e5226c3
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2014-10-20 21:25:32 +0300

    Remove 'vim-byteman'

--- install.bash
+++ install.bash
@@ -117,7 +117,6 @@ VIM_PATHOGEN_URL="https://raw.github.com/tpope/vim-pathogen/master/autoload/path
 download_file $VIM_PATHOGEN_URL ".vim/autoload/pathogen.vim"
 
 HTML5_VIM_URL="https://github.com/othree/html5.vim.git"
-VIM_BYTEMAN_URL="https://github.com/jvirtanen/vim-byteman.git"
 VIM_FUGITIVE_URL="https://github.com/tpope/vim-fugitive.git"
 VIM_GOLANG_URL="https://github.com/jnwhiteh/vim-golang.git"
 VIM_JSON_URL="https://github.com/elzr/vim-json.git"
@@ -130,7 +129,6 @@ VIM_SLEUTH_URL="https://github.com/tpope/vim-sleuth.git"
 VIM_VINEGAR_URL="https://github.com/tpope/vim-vinegar.git"
 
 clone_repository $HTML5_VIM_URL ".vim/bundle/html5.vim"
-clone_repository $VIM_BYTEMAN_URL ".vim/bundle/vim-byteman"
 clone_repository $VIM_FUGITIVE_URL ".vim/bundle/vim-fugitive"
 clone_repository $VIM_GOLANG_URL ".vim/bundle/vim-golang"
 clone_repository $VIM_JSON_URL ".vim/bundle/vim-json"
