abe80ccd85b5cf6b453380ebd4f3ccf64ff1ee0d
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2016-08-28 10:46:51 +0300

    Remove 'kotlin-vim'

--- install.bash
+++ install.bash
@@ -114,7 +114,6 @@ VIM_PATHOGEN_URL="https://raw.github.com/tpope/vim-pathogen/master/autoload/path
 download_file $VIM_PATHOGEN_URL ".vim/autoload/pathogen.vim"
 
 HTML5_VIM_URL="https://github.com/othree/html5.vim.git"
-KOTLIN_VIM_URL="https://github.com/udalov/kotlin-vim.git"
 VIM_ANSIBLE_YAML_URL="https://github.com/chase/vim-ansible-yaml.git"
 VIM_FUGITIVE_URL="https://github.com/tpope/vim-fugitive.git"
 VIM_JSON_URL="https://github.com/elzr/vim-json.git"
@@ -126,7 +125,6 @@ VIM_SLEUTH_URL="https://github.com/tpope/vim-sleuth.git"
 VIM_VINEGAR_URL="https://github.com/tpope/vim-vinegar.git"
 
 clone_repository $HTML5_VIM_URL ".vim/bundle/html5.vim"
-clone_repository $KOTLIN_VIM_URL ".vim/bundle/kotlin-vim"
 clone_repository $VIM_ANSIBLE_YAML_URL ".vim/bundle/vim-ansible-yaml"
 clone_repository $VIM_FUGITIVE_URL ".vim/bundle/vim-fugitive"
 clone_repository $VIM_JSON_URL ".vim/bundle/vim-json"
